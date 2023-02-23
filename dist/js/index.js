b10505c2b503ad5a3ab72f9d9daf748967c30f47
davileal7 <108898208+davileal7@users.noreply.github.com>
2023-02-23 17:52:29 -0300

    tsc primeiros comandos

---
+++ dist/js/index.js
@@ -0,0 +1,17 @@
+"use strict";
+let x = 10;
+x = 25;
+console.log(x);
+let y = 12; //inferencia
+let z = 12; //annotation
+let firstName = "Davi";
+let age = 36;
+const isAdmin = true;
+console.log(typeof firstName);
+firstName = "João";
+console.log(firstName);
+//object
+const myNumbers = [1, 2, 3];
+console.log(myNumbers);
+console.log(myNumbers.length);
+console.log(firstName.toUpperCase());
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+let x: number = 10;
+x = 25;
+console.log(x);
+
+
+let y = 12 //inferencia
+let z: number = 12;  //annotation
+
+let firstName: string = "Davi";
+let age: number = 36;
+const isAdmin: boolean = true;
+
+console.log(typeof firstName)
+firstName = "João"
+console.log(firstName)
+
+//object
+const myNumbers: number[] = [1,2,3]
+console.log(myNumbers)
+console.log(myNumbers.length)
+console.log(firstName.toUpperCase())
